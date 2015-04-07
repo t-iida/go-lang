e54c2c0c85db3c30d6f8c64e6e429cb03babe556
t-iida <dbiida@gmail.com>
2015-04-07 13:57:19 +0900

    Nothing to say.

--- pointer.go
+++ pointer.go
@@ -1,3 +1,9 @@
+/*
+ * Pointer sample
+ *
+ * Passing parameter by using pointer.
+ *
+ */
 package main
 
 import "fmt"
@@ -8,8 +14,6 @@ func inc(value *int) (int) {
     return *value
 }
 
-
-
 func main() {
 
     var intValue int = 0
@@ -18,23 +22,5 @@ func main() {
     fmt.Printf("%v\n", inc(&intValue))
     fmt.Printf("%v\n", inc(&intValue))
     fmt.Printf("%v\n", inc(&intValue))
-
-    /*
-    var intArray = []int {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-
-    for i := 0; i < len(intArray); i++ {
-        fmt.Print(intArray[i]);
-    }
-    fmt.Print("\n")
-
-    for i := 0; i < len(intArray); i++ {
-        intArray[i] = i
-    }
-
-    for i := 0; i < len(intArray); i++ {
-        fmt.Print(intArray[i]);
-    }
-    fmt.Print("\n")
-    */
 
 }
