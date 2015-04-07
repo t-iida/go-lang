6ef822e53c9a40ba725ad602514c81708f497b79
t-iida <dbiida@gmail.com>
2015-04-07 13:55:01 +0900

    Nothing to say.

--- pointer.go
+++ pointer.go
@@ -2,8 +2,24 @@ package main
 
 import "fmt"
 
+// Accepts parameter as pointer
+func inc(value *int) (int) {
+    *value++
+    return *value
+}
+
+
+
 func main() {
 
+    var intValue int = 0
+    fmt.Printf("%v\n", inc(&intValue))
+    fmt.Printf("%v\n", inc(&intValue))
+    fmt.Printf("%v\n", inc(&intValue))
+    fmt.Printf("%v\n", inc(&intValue))
+    fmt.Printf("%v\n", inc(&intValue))
+
+    /*
     var intArray = []int {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 
     for i := 0; i < len(intArray); i++ {
@@ -19,5 +35,6 @@ func main() {
         fmt.Print(intArray[i]);
     }
     fmt.Print("\n")
+    */
 
 }
